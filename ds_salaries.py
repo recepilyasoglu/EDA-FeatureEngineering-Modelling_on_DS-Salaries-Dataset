1dfdecf212c547de22a950ccc2b93bdbdda5ebbe
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-08-04 11:48:37 +0300

    EDA

--- ds_salaries.py
+++ ds_salaries.py
@@ -9,9 +9,9 @@ pd.set_option("display.max_columns", 500)
 df = pd.read_csv("datasets/ds_salaries.csv", index_col=0)
 df.head()
 
-
+# EDA
 def get_stats(dataframe):
-    return ("############### Frist 5 Line ###############", dataframe.head(),
+    return ("############### First 5 Line ###############", dataframe.head(),
             "############### Number of Values Owned ###############", dataframe.value_counts(),
             "############### Total Number of Observations ###############", dataframe.shape,
             "############### Variables Types ############### \n", dataframe.dtypes,
@@ -19,4 +19,45 @@ def get_stats(dataframe):
             "############### Descriptive Statistics ###############", dataframe.describe().T
             )
 
+
 get_stats(df)
+
+df.shape
+df.isnull().sum()
+
+
+def get_value_counts(dataframe):
+    for i in dataframe.columns:
+        print(dataframe[i].value_counts())
+
+get_value_counts(df)
+
+df.groupby(["job_title", "experience_level"]).agg({"salary": ["mean"]})
+
+df.groupby(["job_title"])["salary"].mean().sort_values(ascending=False)
+
+df.groupby(["experience_level"])["salary"].mean().sort_values(ascending=False)
+
+df.groupby("employee_residence")["salary"].mean().sort_values(ascending=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
