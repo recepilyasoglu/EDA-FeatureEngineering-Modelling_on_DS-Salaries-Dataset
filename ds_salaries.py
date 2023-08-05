253c40f636c9ab65c0e6df4a43a7f6a603ee85c2
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2023-08-05 11:43:06 +0300

    Modelling

--- ds_salaries.py
+++ ds_salaries.py
@@ -8,7 +8,6 @@ from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler, Ro
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-from lightgbm import LGBMClassifier
 from sklearn.linear_model import LinearRegression
 
 pd.set_option("display.width", 500)
